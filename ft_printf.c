20975e42b7df6c616995a90613e0a815775ada34
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-01-19 05:49:57 +0100

    WIP

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,49 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 18:44:29 by thugo             #+#    #+#             */
-/*   Updated: 2017/01/17 17:47:09 by thugo            ###   ########.fr       */
+/*   Updated: 2017/01/19 05:04:06 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
+#include <stdio.h> // REMOVE
+#include <unistd.h>
 #include "ft_printf.h"
 
-int		ft_printf(const char *format, ...)
+static void	process_format(const char *format, va_list *ap)
 {
-	va_list		ap;
 	t_parsing	parsing;
 	int			i;
 
-	va_start(ap, format);
 	i = -1;
 	while (format[++i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			i += parse_format(format + i + 1, &parsing, &ap);
-			printf("Attribute: \"%s\" Field width: %d\n", parsing.attr, parsing.field_width);
+			parse_format(format + i + 1, &parsing, ap);
+			convert(&parsing, ap);
+			//printf("Attribute: %d Field width: %d Precision: %d Lmod: %d Conv: %c\n", parsing.attr, parsing.field_width, parsing.precision, parsing.lmod, parsing.conv_spec);
 			//printf("format: %s\n", format + i);
 		}
 		//else
 			// Ajoute dans le buffer tant qu'il n'y a pas de %
 	}
+}
+
+int			ft_printf(const char *format, ...)
+{
+	va_list		ap;
+	size_t		nbytes;
+	size_t		nchars;
+	char		*str;
+
+	buffer_init();
+	va_start(ap, format);
+	process_format(format, &ap);
 	va_end(ap);
-	return (0);
+	buffer_getinfo(&nbytes, &nchars);
+	str = buffer_get();
+	buffer_clear();
+	if (nbytes > 0)
+		write(1, str, nbytes);
+	return (nchars);
 }
--- main.c
+++ main.c
@@ -6,14 +6,33 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 18:44:43 by thugo             #+#    #+#             */
-/*   Updated: 2017/01/17 17:49:03 by thugo            ###   ########.fr       */
+/*   Updated: 2017/01/19 05:23:53 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "ft_printf.h"
+#include <stdio.h>
+#include <unistd.h>
 
 int	main()
 {
-	ft_printf("%##*", 58);
+	/*long long a = 18446744073709551615U;
+	unsigned long long b = a;
+	//unsigned long long c = b;
+	printf("%llu\n", b);*/
+	char	*str;
+	size_t	size;
+	
+	ft_printf("%d", -1);
+	
+	/*buffer_init();
+	buffer_add("salut", 5, 5);
+	buffer_add(" ca va?", 7, 7);
+	str = buffer_get();
+	buffer_getinfo(&size, NULL);
+	write(1, str, size);
+	buffer_clear();
+	free(str);*/
 	return (0);
 }
