e99b833509c41f9a5ca690eb72c644b2446f0184
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-01-20 09:07:17 +0100

    Add new param arg

--- vaargs.c
+++ vaargs.c
@@ -6,44 +6,45 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/20 00:26:45 by thugo             #+#    #+#             */
-/*   Updated: 2017/01/20 00:27:22 by thugo            ###   ########.fr       */
+/*   Updated: 2017/01/20 05:24:42 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-long long			get_ll_arg(t_parsing *p, va_list *ap)
+long long			get_ll_arg(t_parsing *p, va_list *ap, long long *arg)
 {
 	if (p->lmod == LMOD_H)
-		return ((short)va_arg(*ap, int));
+		return ((*arg = (short)va_arg(*ap, int)));
 	else if (p->lmod == LMOD_HH)
-		return ((char)va_arg(*ap, int));
+		return ((*arg = (char)va_arg(*ap, int)));
 	else if (p->lmod == LMOD_L)
-		return (va_arg(*ap, long));
+		return ((*arg = va_arg(*ap, long)));
 	else if (p->lmod == LMOD_LL)
-		return (va_arg(*ap, long long));
+		return ((*arg = va_arg(*ap, long long)));
 	else if (p->lmod == LMOD_J)
-		return (va_arg(*ap, intmax_t));
+		return ((*arg = va_arg(*ap, intmax_t)));
 	else if (p->lmod == LMOD_Z)
-		return (va_arg(*ap, size_t));
+		return ((*arg = va_arg(*ap, size_t)));
 	else
-		return (va_arg(*ap, int));
+		return ((*arg = va_arg(*ap, int)));
 }
 
-unsigned long long	get_llu_arg(t_parsing *p, va_list *ap)
+unsigned long long	get_llu_arg(t_parsing *p, va_list *ap,
+		unsigned long long *arg)
 {
 	if (p->lmod == LMOD_H)
-		return ((unsigned short)va_arg(*ap, unsigned int));
+		return ((*arg = (unsigned short)va_arg(*ap, unsigned int)));
 	else if (p->lmod == LMOD_HH)
-		return ((unsigned char)va_arg(*ap, unsigned int));
+		return ((*arg = (unsigned char)va_arg(*ap, unsigned int)));
 	else if (p->lmod == LMOD_L)
-		return (va_arg(*ap, unsigned long));
+		return ((*arg = va_arg(*ap, unsigned long)));
 	else if (p->lmod == LMOD_LL)
-		return (va_arg(*ap, unsigned long long));
+		return ((*arg = va_arg(*ap, unsigned long long)));
 	else if (p->lmod == LMOD_J)
-		return (va_arg(*ap, intmax_t));
+		return ((*arg = va_arg(*ap, intmax_t)));
 	else if (p->lmod == LMOD_Z)
-		return (va_arg(*ap, size_t));
+		return ((*arg = va_arg(*ap, size_t)));
 	else
-		return (va_arg(*ap, unsigned int));
+		return ((*arg = va_arg(*ap, unsigned int)));
 }
