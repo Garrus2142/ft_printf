a837c14bf71dfd9a32f72a3bd80c794e3085f0a0
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-01-25 18:52:51 +0100

    Finish extra

--- convert_extra.c
+++ convert_extra.c
@@ -6,7 +6,7 @@
 /*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 16:20:33 by thugo             #+#    #+#             */
-/*   Updated: 2017/01/25 18:12:03 by thugo            ###   ########.fr       */
+/*   Updated: 2017/01/25 18:49:11 by thugo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ static void	process_n(t_parsing *p, va_list *ap)
 		*(va_arg(*ap, int *)) = (int)buffer_getinfo();
 }
 
-static void	process_rR(t_parsing *p, va_list *ap)
+static void	process_yY(t_parsing *p, va_list *ap)
 {
 	char	*buffer;
 
 	buffer = buffer_get();
-	if (p->conv_spec == 'r')
+	if (p->conv_spec == 'y')
 		*(va_arg(*ap, char **)) = buffer;
 	else
 		write(va_arg(*ap, int), buffer, buffer_getinfo());
@@ -49,11 +49,9 @@ static void	process_rR(t_parsing *p, va_list *ap)
 
 void		convert_extra(t_parsing *p, va_list *ap, size_t *nbytes)
 {
-	char	*str;
-
 	if (p->conv_spec == 'n')
 		process_n(p, ap);
-	else if (p->conv_spec == 'r' || p->conv_spec == 'R')
-		process_rR(p, ap);
+	else if (p->conv_spec == 'y' || p->conv_spec == 'Y')
+		process_yY(p, ap);
 	*nbytes = 0;
 }
